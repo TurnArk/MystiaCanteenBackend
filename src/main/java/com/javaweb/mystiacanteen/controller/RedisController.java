873ae266f51dc086c03c1ae0e2d31734cdcc4641
TurnArk <1716236031@qq.com>
2025-06-28 23:26:16 +0800

    初步完成加锁问题，未测试，在补这一部分前端

--- src/main/java/com/javaweb/mystiacanteen/controller/RedisController.java
+++ src/main/java/com/javaweb/mystiacanteen/controller/RedisController.java
@@ -4,12 +4,11 @@ import com.javaweb.mystiacanteen.entity.CartData;
 import com.javaweb.mystiacanteen.entity.Production;
 import com.javaweb.mystiacanteen.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/redis")
@@ -17,20 +16,55 @@ public class RedisController {
     @Autowired
     private RedisService redisService;
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @GetMapping("/getAllProduct")
     public List<Production> getAllProduct()
     {
         return redisService.getAllProduct();
     }
 
-    @PostMapping("/renew")
-    public String renew(String username, CartData cartData)
+    @PostMapping("/renew/{username}")
+    public String renew(@PathVariable String username,@RequestBody CartData cartData)
     {
-        return redisService.updateProduct(username, cartData) ? "更新成功" : "更新失败";
+        String limitKey = "qps:limit";
+        Long count = redisTemplate.opsForValue().increment(limitKey);
+        if(count == 1){
+            redisTemplate.expire(limitKey, 1, TimeUnit.SECONDS);
+        }
+        if(count > 100){
+            return "操作过于频繁";
+        }
+        String lockKey = "lock:id:2011";
+        Boolean lock = redisTemplate.opsForValue()
+                .setIfAbsent(lockKey, "1", 3, TimeUnit.SECONDS);
+
+        if (!lock) {
+            return "系统繁忙，请稍后再试";
+        }
+        try {
+            Integer stock = (Integer) redisTemplate.opsForValue().get("id:2011");
+            if (stock == null || stock <= 0) {
+                return "库存不足";
+            }
+
+            Long newStock = redisTemplate.opsForValue().decrement("id:2011");
+            if (newStock < 0) {
+                // 回滚超减
+                redisTemplate.opsForValue().increment("id:2011");
+                return "库存不足";
+            }
+            // TODO：下单入库逻辑
+            return redisService.updateProduct(username, cartData) ?"秒杀成功！库存剩余：" + newStock:"更新失败";
+
+        } finally {
+            redisTemplate.delete(lockKey);
+        }
     }
 
-    @PostMapping("/delete")
-    public String delete(String username, CartData cartData){
+    @PostMapping("/delete/{username}")
+    public String delete(@PathVariable String username,@RequestBody CartData cartData){
         return redisService.deleteProduct(username, cartData) ? "删除成功" : "删除失败";
     }
 }
